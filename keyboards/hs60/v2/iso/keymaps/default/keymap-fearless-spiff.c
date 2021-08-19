a3a0f6aff2c688f3fbf483641e7a526a72299037
FearlessSpiff <fearless.spiff@me.com>
2021-08-19 22:06:01 +0200

    added first draft of new "40 percent version" for hs60

--- keyboards/hs60/v2/iso/keymaps/default/keymap-fearless-spiff.c
+++ keyboards/hs60/v2/iso/keymaps/default/keymap-fearless-spiff.c
@@ -14,7 +14,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include QMK_KEYBOARD_H
-#include "keymap_german.h"
+#include keymap_extras/keymap_german.h
 
 //This is the ISO version of the PCB
 
