b5dcd9f4f5524c62636240fc81736fbbbcdcb84c
FearlessSpiff <fearless.spiff@me.com>
2021-09-15 21:44:05 +0200

    changed some hs60 layouts to be more like the 40 percent ones

--- keyboards/hs60/v2/iso/keymaps/default/keymap-fearless-spiff.c
+++ keyboards/hs60/v2/iso/keymaps/default/keymap-fearless-spiff.c
@@ -22,30 +22,30 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [0] = LAYOUT_60_iso( /* Base */
     KC_GESC,  KC_1,     KC_2,     KC_3,  KC_4,  KC_5,  KC_6,    KC_7,  KC_8,  KC_9,     KC_0,     DE_SS,   DE_ACUT,  KC_BSPC,\
     KC_TAB,   KC_Q,     KC_W,     KC_E,  KC_R,  KC_T,  KC_Y,    KC_U,  KC_I,  KC_O,     KC_P,     DE_UDIA,  DE_PLUS,         \
-    MO(1),    KC_A,     KC_S,     KC_D,  KC_F,  KC_G,  KC_H,    KC_J,  KC_K,  KC_L,     DE_ODIA,  DE_ADIA,  DE_HASH, KC_ENT, \
+    LT(1, KC_TAB),    KC_A,     KC_S,     KC_D,  KC_F,  KC_G,  KC_H,    KC_J,  KC_K,  KC_L,     DE_ODIA,  DE_ADIA,  DE_HASH, KC_ENT, \
     KC_LSFT,  DE_LABK,  KC_Z,     KC_X,  KC_C,  KC_V,  KC_B,    KC_N,  KC_M,  DE_COMM,  DE_DOT,   DE_MINS,           KC_RSFT,\
     KC_LCTL,  KC_LGUI,  KC_LALT,                       LT(2,KC_SPC),                    KC_RALT,  KC_CAPS,  KC_APP,  KC_RCTL),
 
 [1] = LAYOUT_60_iso( /* FN */
     KC_GRV,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_DEL ,\
-    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  DE_LCBR,  DE_RCBR,  KC_TRNS,  DE_UDIA,  KC_UP,    DE_ODIA,  KC_TRNS,  KC_TRNS,  KC_TRNS  ,          \
-    KC_TRNS,  DE_ADIA,  DE_LABK,  DE_RABK,  DE_LPRN,  DE_RPRN,  KC_TRNS,  KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,\
-    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  DE_LBRC,  DE_RBRC,  KC_TRNS,  KC_HOME,  KC_END,   KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,\
-    KC_VOLU,  KC_VOLD,  KC_MUTE,                                KC_TRNS,                                KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS),
+    _______,  _______,  _______,  _______,  DE_LCBR,  DE_RCBR,  _______,  DE_UDIA,  KC_UP,    DE_ODIA,  _______,  _______,  _______  ,          \
+    _______,  DE_ADIA,  DE_SS,    _______,  DE_LPRN,  DE_RPRN,  _______,  KC_LEFT,  KC_DOWN,  KC_RGHT,  _______,  _______,  _______,  _______,\
+    _______,  _______,  _______,  _______,  DE_LBRC,  DE_RBRC,  _______,  KC_HOME,  KC_END,   _______,  _______,  _______,            _______,\
+    _______,  _______,  _______,                                _______,                                _______,  _______,  _______,  _______),
 
 [2] = LAYOUT_60_iso( /* Empty for dynamic keymaps */
-    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,\
-    KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_TRNS,          \
-    KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,\
-    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,\
-    KC_TRNS,  KC_TRNS,  KC_TRNS,                                KC_TRNS,                                KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS),
+    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,\
+    KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   _______,          \
+    KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     _______,  _______,  _______,  _______,\
+    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,\
+    _______,  _______,  _______,                                _______,                                _______,  _______,  _______,  _______),
 
 [3] = LAYOUT_60_iso( /* Empty for dynamic keymaps */
-    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,\
-    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,          \
-    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,\
-    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,\
-    KC_TRNS,  KC_TRNS,  KC_TRNS,                                KC_TRNS,                                KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS),
+    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,\
+    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,          \
+    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,\
+    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,\
+    _______,  _______,  _______,                                _______,                                _______,  _______,  _______,  _______),
 };
 
 void matrix_init_user(void) {
