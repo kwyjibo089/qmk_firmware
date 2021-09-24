2e20ed1424ba27b933fa71b2f96fb451341a0c14
FearlessSpiff <fearless.spiff@me.com>
2021-09-24 16:52:05 +0200

    added some bugfixes and improvements to mercutio bongocat_fearless_spiff

--- keyboards/mechwild/mercutio/keymaps/bongocat_fearless_spiff/keymap.c
+++ keyboards/mechwild/mercutio/keymaps/bongocat_fearless_spiff/keymap.c
@@ -39,23 +39,23 @@ uint8_t current_tap_frame = 0;
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_all(
                                                                                                                     KC_MUTE,
-        KC_GESC,          KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Z,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+        KC_GESC,          KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    DE_Z,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
         LT(1,KC_TAB),     KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    DE_HASH, KC_ENT,
-        KC_LSFT, DE_LABK, KC_Y,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,           KC_RSFT,
+        KC_LSFT, DE_LABK, DE_Y,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,           KC_RSFT,
         KC_LCTL, KC_LGUI, KC_LALT,   LT(2,KC_SPC),  LT(2,KC_SPC),   LT(2,KC_SPC),        KC_RALT, DE_MINS,          KC_RCTL ),
 
     [1] = LAYOUT_all(
-                                                                                                                     _______,
-    	  _______,          _______, _______, _______, DE_LCBR, DE_RCBR, _______, DE_UDIA, KC_UP,   DE_ODIA, DE_QUES, KC_DEL,
+                                                                                                                      _______,
+    	  _______,          _______, _______, _______, DE_LCBR, DE_RCBR, _______, DE_UDIA, KC_UP,    DE_ODIA, DE_QUES, KC_DEL,
     	  _______,          DE_ADIA, DE_SS,   _______, DE_LPRN, DE_RPRN, _______, KC_LEFT, KC_DOWN, KC_RIGHT, DE_PLUS, _______,
-    	  _______, _______, _______, _______, _______, DE_LBRC, DE_RBRC, _______, KC_HOME, KC_END,  _______,          _______,
-    	  _______, _______, _______,          _______, _______,          _______,          DE_BSLS, DE_SLSH,          _______ ),
+    	  _______, _______, DE_ACUT, _______, _______, DE_LBRC, DE_RBRC, KC_HOME, KC_END,  DE_QUOT,  DE_DQUO,          _______,
+    	  RESET,   _______, _______,          _______, _______,          _______,          DE_BSLS, DE_SLSH,          DE_TILD ),
 
     [2] = LAYOUT_all(
-                                                                                                                    _______,
+                                                                                                                      _______,
     	  KC_F1,            KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_BSPC,
     	  KC_1,             KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______, _______,
-    	  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
+    	  _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_COMM, KC_DOT,           _______,
     	  _______, _______, _______,          _______, _______,          _______,          _______, _______,          _______ )
 };
 
