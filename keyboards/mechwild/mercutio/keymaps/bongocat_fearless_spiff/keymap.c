a2d03e40b7f3f5782743a07c176cb428e31c2ae3
FearlessSpiff <fearless.spiff@me.com>
2021-10-09 21:33:36 +0200

    first usable own code for encoder and oled

--- keyboards/mechwild/mercutio/keymaps/bongocat_fearless_spiff/keymap.c
+++ keyboards/mechwild/mercutio/keymaps/bongocat_fearless_spiff/keymap.c
@@ -17,6 +17,7 @@
 #include QMK_KEYBOARD_H
 #include "keymap_german.h"
 #include <stdio.h>
+#include "print.h"
 
 // OLED setup for bongocat
 #define IDLE_FRAMES 5
@@ -26,7 +27,21 @@
 #define ANIM_FRAME_DURATION 200
 #define ANIM_SIZE 512
 
-static long int oled_timeout = 300000; // 5 minutes
+#define QWERTZ 0
+#define FN_1 1
+#define FN_2 2
+
+#define LABEL_LENGTH 8
+
+#define KC_CAD	        LALT(LCTL(KC_DEL))
+#define KC_TEAMS_CAM	C(S(KC_O))
+#define KC_TEAMS_MUTE	C(S(KC_M))
+
+enum custom_user_keycodes {
+  KC_ENC = SAFE_RANGE
+};
+
+static long int oled_timeout = 1800000; //  30 minutes
 bool gui_on = true;
 char wpm_str[10];
 uint32_t anim_timer = 0;
@@ -34,24 +49,81 @@ uint32_t anim_sleep = 0;
 uint8_t current_idle_frame = 0;
 uint8_t current_tap_frame = 0;
 
+// Tap Dance declarations
+enum {
+    TD_TAB_ESC,
+};
+
+// Tap Dance definitions
+qk_tap_dance_action_t tap_dance_actions[] = {
+    // Tap once for Tab, twice for Esc
+    [TD_TAB_ESC] = ACTION_TAP_DANCE_DOUBLE(KC_TAB, KC_ESC),
+};
+
+typedef struct {
+    char label_top[LABEL_LENGTH];
+    char label_mid[LABEL_LENGTH];
+    char label_bottom[LABEL_LENGTH];
+    uint16_t keycode;
+} encoder_key;
+
+static const encoder_key PROGMEM encoder_keys[] = {
+    // list of key codes that will be scrolled through by encoder and description
+    // Be aware that it only works when using one char less than defined. In this case 7 max. No idea why?
+    {"Teams", "Toggle", "Mic", KC_TEAMS_MUTE},
+    {"Teams", "Toggle", "Cam", KC_TEAMS_CAM},
+    {"Ctrl", "Alt", "Del", KC_CAD},
+    {"Caps", "Lock", "", KC_CAPSLOCK},
+    {"Pause", "", "", KC_PAUSE},
+    {"PrtScr", "", "", KC_PSCR},
+    {"Insert", "", "", KC_INS},
+    {"Play", "", "", KC_MEDIA_PLAY_PAUSE}
+};
+
+// TODO can't this be simplified? Should be sizeof(encoder_keys) only?
+#define NUMBER_OF_ENCODER_KEYS sizeof(encoder_keys)/sizeof(encoder_keys[0])
+
+static uint8_t selected_encoder_key_id = 0;
+static encoder_key selected_encoder_key;
+
+static void set_selected_encoder_key(uint8_t idx) {
+    // make a copy from PROGMEM
+    memcpy_P (&selected_encoder_key, &encoder_keys[idx], sizeof selected_encoder_key);
+}
+
+void keyboard_post_init_keymap(void) {
+    // Call the keyboard post init code.
+    set_selected_encoder_key(selected_encoder_key_id);
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case KC_ENC:
+            if (record->event.pressed) {
+                tap_code16(selected_encoder_key.keycode);
+            }
+        break;
+    }
+    return true;
+}
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [0] = LAYOUT_all(
-                                                                                                                    KC_MUTE,
-        KC_GESC,          KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    DE_Z,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-        LT(1,KC_TAB),     KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    DE_PLUS, KC_ENT,
+  [QWERTZ] = LAYOUT_all(
+                                                                                                                    KC_ENC,
+        TD(TD_TAB_ESC),          KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    DE_Z,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+        MO(1),     KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    DE_PLUS, KC_ENT,
         KC_LSFT, DE_LABK, DE_Y,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,           KC_RSFT,
         KC_LCTL, KC_LGUI, KC_LALT,   LT(2,KC_SPC),  LT(2,KC_SPC),   LT(2,KC_SPC),        KC_RALT, DE_MINS,          KC_RCTL ),
 
-    [1] = LAYOUT_all(
-                                                                                                                      _______,
-    	  _______,          _______, _______, _______, DE_LCBR, DE_RCBR, _______, DE_UDIA, KC_UP,    DE_ODIA, DE_QUES, KC_DEL,
+    [FN_1] = LAYOUT_all(
+                                                                                                                      KC_MUTE,
+    	  KC_GESC,          _______, _______, _______, DE_LCBR, DE_RCBR, _______, DE_UDIA, KC_UP,    DE_ODIA, DE_QUES, KC_DEL,
     	  _______,          DE_ADIA, DE_SS,   _______, DE_LPRN, DE_RPRN, _______, KC_LEFT, KC_DOWN, KC_RIGHT, DE_HASH, _______,
-    	  _______, _______, DE_ACUT, _______, _______, DE_LBRC, DE_RBRC, KC_HOME, KC_END,  DE_QUOT,  DE_DQUO,          DE_EQL,
+    	  _______, DE_CIRC, DE_ACUT, _______, _______, DE_LBRC, DE_RBRC, KC_HOME, KC_END,  DE_QUOT,  DE_DQUO,          DE_EQL,
     	  _______,   _______, _______,          _______, _______,          _______,          DE_BSLS, DE_SLSH,          DE_TILD ),
 
-    [2] = LAYOUT_all(
+    [FN_2] = LAYOUT_all(
                                                                                                                       _______,
     	  KC_F1,            KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_BSPC,
     	  KC_1,             KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______, _______,
@@ -61,30 +133,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 #ifdef ENCODER_ENABLE
 bool encoder_update_user(uint8_t index, bool clockwise) {
-    switch (index) {
-        case 0:
-            if (clockwise) {
-                if (IS_LAYER_ON(0)) {
-                    tap_code16(C(KC_RGHT));
-                }
-                if (IS_LAYER_ON(1)) {
-                    tap_code(KC_VOLU);
-                }
-                if (IS_LAYER_ON(2)) {
-                    tap_code16(C(S(KC_O)));
-                }
-            } else {
-                if (IS_LAYER_ON(0)) {
-                    tap_code16(C(KC_LEFT));
-                }
-                if (IS_LAYER_ON(1)) {
-                    tap_code(KC_VOLD);
-                }
-                if (IS_LAYER_ON(2)) {
-                    tap_code16(C(S(KC_M)));
-                }
-            }
-        break;
+    if (clockwise) {
+        if (IS_LAYER_ON(QWERTZ) && selected_encoder_key_id  < NUMBER_OF_ENCODER_KEYS-1) {
+           selected_encoder_key_id ++;
+           set_selected_encoder_key(selected_encoder_key_id);
+        }
+        if (IS_LAYER_ON(FN_1)) {
+            tap_code(KC_VOLU);
+        }
+        if (IS_LAYER_ON(FN_2)) {
+            tap_code16(KC_PGDOWN);
+        }
+    } else {
+        if (IS_LAYER_ON(QWERTZ) && selected_encoder_key_id  > 0) {
+            selected_encoder_key_id --;
+            set_selected_encoder_key(selected_encoder_key_id);
+        }
+        if (IS_LAYER_ON(FN_1)) {
+            tap_code(KC_VOLD);
+        }
+        if (IS_LAYER_ON(FN_2)) {
+            tap_code16(KC_PGUP);
+        }
     }
     return true;
 }
@@ -119,7 +189,7 @@ static void render_anim(void) {
         },
 
         {
-            0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,128,128,0,0,0,0,0,128,64,64,32,32,32,32,16,16,16,16,8,4,2,1,1,2,12,48,64,128,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,128,128,128,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,30,225,0,0,1,1,2,2,1,0,0,0,0,128,128,0,0,0,0,0,0,0,0,0,128,0,48,48,0,192,193,193,194,4,8,16,32,64,128,0,0,0,128,128,128,128,64,64,
+            0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,128,128,0,0,0,0,0,128,64,64,32,32,32,32,16,16,16,16,8,4,2,1,1,2,12,48,64,128,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,128,128,128,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,30,225,0,0,1,1,2,2,1,0,0,0,0,128,128,0,0,0,0,0,0,0,0,0,128,0,48,48,0,192,193,193,194,4,8,16,32,64,128,0,0,0,128,128,128,128,64,64,
             64,64,32,32,32,32,16,16,16,16,8,8,8,8,8,4,4,4,4,4,2,2,2,2,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,128,112,12,3,0,0,0,0,0,0,0,12,12,12,13,1,0,64,160,33,34,18,17,17,17,9,8,8,8,8,4,4,8,8,16,16,16,16,16,17,15,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,128,128,128,128,128,64,64,64,64,64,32,32,32,32,32,16,16,16,16,16,8,8,8,8,8,4,4,4,4,4,2,3,2,2,1,1,1,1,1,1,2,2,4,4,8,8,8,8,8,
             7,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
         },
@@ -161,16 +231,16 @@ static void render_anim(void) {
     };
 
     void animation_phase(void) {
-        if (get_current_wpm() <=IDLE_SPEED) {
+        if (get_current_wpm() <= IDLE_SPEED) {
             current_idle_frame = (current_idle_frame + 1) % IDLE_FRAMES;
             oled_write_raw_P(idle[abs((IDLE_FRAMES-1)-current_idle_frame)], ANIM_SIZE);
         }
 
-        if (get_current_wpm() >IDLE_SPEED && get_current_wpm() <TAP_SPEED) {
+        if (get_current_wpm() > IDLE_SPEED && get_current_wpm() < TAP_SPEED) {
             oled_write_raw_P(prep[0], ANIM_SIZE);
         }
 
-        if (get_current_wpm() >=TAP_SPEED) {
+        if (get_current_wpm() >= TAP_SPEED) {
             current_tap_frame = (current_tap_frame + 1) % TAP_FRAMES;
             oled_write_raw_P(tap[abs((TAP_FRAMES-1)-current_tap_frame)], ANIM_SIZE);
         }
@@ -198,11 +268,23 @@ static void render_anim(void) {
 }
 
 void oled_task_user(void) {
+
         render_anim();
-        oled_set_cursor(0,4);
+
+        oled_set_cursor(0,0);
+        oled_write(selected_encoder_key.label_top, false);
+
+        oled_set_cursor(0,1);
+        oled_write(selected_encoder_key.label_mid, false);
+
+        oled_set_cursor(0,2);
+        oled_write(selected_encoder_key.label_bottom, false);
+
+/* hide wpm display for now
+        oled_set_cursor(13,3);
         sprintf(wpm_str, "WPM: %03d", get_current_wpm());
         oled_write(wpm_str, false);
-
+*/
 }
 
 #endif
